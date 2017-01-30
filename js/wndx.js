a2f9a04527fb80cbac3a32ef169e6dae3a8c0254
Lori M Olson <lori@wndx.com>
2017-01-30 16:15:58 -0700

    Add a bootstrap alert plugin, so I can make any blog page into my "post signup target" page from drip

--- js/wndx.js
+++ js/wndx.js
@@ -3,10 +3,22 @@
 // to each of the social media share links
 //
 
-$(function() {
-    $('ul.social-buttons a').on('click', function() {
-        // Get the classes off the i child element of $anchor for the actual social network
-        // var $anchor = $(this);
-        ga('send', 'pageview', '/vpv/social');
-    });
+$(function () {
+  $('ul.social-buttons a').on('click', function () {
+    // Get the classes off the i child element of $anchor for the actual social network
+    // var $anchor = $(this);
+    ga('send', 'pageview', '/vpv/social');
+  });
+
+  if (window.location.hash) {
+    var hash = window.location.hash.substring(1);
+    if (hash == "coredatathanks") {
+      $(document).trigger("add-alerts", [
+        {
+          "message": "Here is some additional RubyMotion content I hope you find useful.",
+          "priority": 'info'
+        }
+      ]);
+    }
+  }
 });
